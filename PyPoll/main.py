b07d74d0eb372ed003a6d147dd66e9646de0e7c2
Mark Reilly <mdr1234@yahoo.com>
2019-03-02 22:28:27 -0500

    updating main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,3 +3,17 @@ import os
 import csv
 # convert csv
 election = os.path.join ("resources","election_data.csv")
+with open(election, 'r') as csvfile:
+    data = csvfile
+    csvreader = csv.reader(data, delimiter=',')
+
+# The total number of votes included in the dataset
+    row_count = sum(1 for row in csvreader)-1
+
+# Reset csvreader
+    data.seek(0)
+# Print total votes
+    print(row_count)
+    data.seek(0)
+
+    
